070b4d5cf1f9f44e4be0db5d2cf8a0b3bedccda3
Isand63 <113596832+Isand63@users.noreply.github.com>
2022-11-11 16:46:56 +0300

    все коды
    В 2022 году есть код не в word файле в папке под названием temp

---
+++ мои коды/2022/C++/temp/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int c = 7;
+    double artemp[c];
+    cout << "Введите температуру\n";
+    for (int i = 0; i < c; i++)
+    {
+        cout << "Температура в " << (i + 1) << " день ";
+        cin >> artemp[i];
+    }
+    float avtemp = 0, sumtemp = 0, maxtemp = 0;
+    int countdays = 0;
+    for (int i = 0; i < c; i++)
+    {
+        sumtemp += artemp[i];
+        if (artemp[i] > maxtemp)
+        {
+            maxtemp = artemp[i];
+            countdays = 1;
+        }
+        else if (artemp[i] == maxtemp)countdays++;
+    }
+    avtemp = sumtemp / c;
+    cout << "Средняя температура " << avtemp << endl;
+
+    ofstream out;
+    out.open("C:\\work\\Htemp.txt");
+    if (out.is_open())
+    {
+        out <<"Средняя температура "<< avtemp<<endl;
+    }
+    system("pause");
+    return 0;
+}
